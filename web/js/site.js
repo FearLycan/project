ecaf3d154a49e6dc84fb05766f0e864ef0c9993b
Damian Brończyk <damian.bronczyk@tauceti.email>
2018-05-28 21:56:59 +0200

    update

--- web/js/site.js
+++ web/js/site.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
 
 var options = {
     url: function (phrase) {
-        return "/site/json?phrase=" + encodeURIComponent(phrase);
+        return "/project/web/site/json?phrase=" + encodeURIComponent(phrase);
     },
     getValue: function (element) {
         return element.name;
@@ -40,44 +40,52 @@ var options = {
         type: "custom",
         method: function (title, item) {
 
-            if(item.type == 'tag'){
+            if (item.type == 'tag') {
                 return '<div class="row">' +
-                    '<div class="col-2">' +
+                    '<div class="col-4 s-title">' +
                     item.name
                     + '</div>' +
+                    '<div class="col-1">' +
+                    '<small class="s-small">tag</small>' +
+                    '</div>' +
                     '<div class="col-2">' +
                     '<a href="/project/web/tag/' + item.name + '">wyszukaj</a>'
                     + '</div>'
                     + '</div>';
             }
 
-            if(item.type == 'shop'){
+            if (item.type == 'shop') {
                 return '<div class="row">' +
-                    '<div class="col-2">' +
+                    '<div class="col-4 s-title">' +
                     item.name
                     + '</div>' +
+                    '<div class="col-1">' +
+                    '<small class="s-small">sklep</small>' +
+                    '</div>' +
                     '<div class="col-2">' +
                     '<a href="/project/web/shop/' + item.slug + '">zobacz</a>'
                     + '</div>'
                     + '</div>';
             }
 
-            if(item.type == 'item'){
+            if (item.type == 'item') {
                 return '<div class="row">' +
-                    '<div class="col-2">' +
+                    '<div class="col-4 s-title">' +
                     title
                     + '</div>' +
+                    '<div class="col-1">' +
+                    '<small class="s-small">przedmiot</small>' +
+                    '</div>' +
                     '<div class="col-2">' +
-                    '<a href="/project/web/tag/' + item.name + '">wyszukaj</a>'
+                    '<a href="/project/web/item/' + item.id + '/' + item.slug + '">zobacz</a>'
                     + '</div>'
                     + '</div>';
             }
 
 
-
         }
     },
-    requestDelay: 400
+    requestDelay: 300
 };
 
 $("#search-input").easyAutocomplete(options);
