3ee8fd16d5da541b7cd53eb0bc09b2f0ff10ea14
Damian Brończyk <damian.bronczyk@tauceti.email>
2018-06-12 09:19:35 +0200

    update

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -34,13 +34,6 @@ AppAsset::register($this);
     <!-- Favicon -->
     <link rel="shortcut icon" href="<?php echo Yii::$app->request->baseUrl; ?>/images/icon/favicon.ico" type="image/x-icon" />
 
-    <style>.async-hide { opacity: 0 !important} </style>
-    <script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
-            h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
-            (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
-        })(window,document.documentElement,'async-hide','dataLayer',4000,
-            {'GTM-W4XCHGQ':true});</script>
-
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-84680217-3"></script>
     <script>
@@ -49,16 +42,6 @@ AppAsset::register($this);
         gtag('js', new Date());
 
         gtag('config', 'UA-84680217-3');
-    </script>
-
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-84680217-3', 'auto');
-        ga('require', 'GTM-W4XCHGQ');
-        ga('send', 'pageview');
     </script>
 
     <!-- Fonts -->
